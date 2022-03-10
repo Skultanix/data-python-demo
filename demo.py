73527acec92990310a4d6938fd0b2f08b4f108c1
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-10 13:06:33 -0700

    Demo complete.

--- demo.py
+++ demo.py
@@ -1,78 +1,66 @@
-# Python does not require a key word to declare a value as a variable.
-first_name = 'Cameron'
-print(first_name)
+from functools import total_ordering
 
-# Strings can be set with single or double quotes.
-last_name = "Stuart"
+
+first_name = 'Caleb'
+last_name = "Ricks"
+print(first_name)
 print(last_name)
 
-# Note, like JavaScript, Python variables can change type.
-age = 'seventy-six'
-age = 17
-print(age)
+# by no means is this list comprehensive
+favorite_games = '''
+Skyrim, 
+Diablo II, 
+Warcraft III, 
+Northgard, and 
+Ark: Survival Evolved
+'''
+print(favorite_games)
 
-# Use the type function to see the type of a given value.
-print(type(first_name))
+age = 25
+hourly_rate = 420.69
+likes_dogs = True
+
 print(type(age))
+print(type(likes_dogs))
+print(type(hourly_rate))
 
-# Comments are set with the octothorpe (pound, hashtag).
+if likes_dogs == True:
+    print('You have a soul.')
 
-# If Statements
-if age >= 18:
-  print('I am an adult.')
-
-# If-else Statements
-if age >= 18:
-  print('I am an adult.')
-else:
-  print('I am a child.')
-
-#If-elif-else
-if age >= 18:
-  print('I am an adult.')
-elif age < 13:
-  print('I am a child.')
-else:
-  print('I am a teen.')
-
-## Note: above code would break if you had two blank lines in between code.
-
-# For-In-Loop (similar to JavaScript for-loop)
-for x in [1,2,3,4,5,6,7,8]:
-  print(x)
-
-# Another example of for-in loop, also demonstrating creation of array and use of a built in method, just like you would do in JavaScript.
-instructors = ['cameron', 'riley', 'eric', 'nitin']
-for cool_cat in instructors:
-  print(cool_cat.capitalize())
-
-# Handling Data with Python
-open_file = open('FinalGrades.csv')
-
-for row in open_file:
-  print(row)
-
-open_file.close()
-
-# A more complicated look at what we can do with the data.
 open_file = open("FinalGrades.csv")
+
 total_a = 0
-total_b = 0
+total_b =0
 total_c = 0
 
 for line in open_file:
-  line = line.rstrip('\n').split(',')
-  for value in line: 
-    if value == "A": 
-      total_a += 1
-    elif value == "B":
-      total_b += 1
-    elif value == "C": 
-      total_c += 1
-
+    line = line.rstrip('\n').split(',')
+    for value in line:
+        if value == 'A':
+            total_a +=1
+        elif value == "B":
+            total_b +=1
+        elif value == "C":
+            total_c +=1
 print("A's:", total_a)
-print("B's:", total_b)
-print("C's:", total_c)
-
+print("B's:",total_b)
+print("C's:",total_c)
+print(line[len(line) - 1])
 open_file.close()
 
+open_file2 = open("orders.csv")
+
+big_orders = 0
+small_orders = 0
+
+for line in open_file2:
+    line = line.rstrip('\n').split(',')
+    for value in line:
+        if int(line[1]) > 30:
+            big_orders +=1
+        else:
+            small_orders +=1
+open_file2.close()
+
+print(big_orders)
+print(small_orders)
